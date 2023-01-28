68666939319068c21393cda12ada11f1e301e170
Sanjay R. Kharche, Ph.D <srk.london@yahoo.com>
2023-01-28 12:09:23 -0500

    Delete sources directory

--- sources/d.MutualInfoDataGeneration/May2011/baselinerun.sh
+++
@@ -1,6 +0,0 @@
-# make directorries and run the san binary
-rm san
-# cc -o san mousesan_E4031.c -lm
-cc -o san mousesan.c -lm
-#
-./san 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
--- sources/d.MutualInfoDataGeneration/May2011/delete.sh
+++
@@ -1,5 +0,0 @@
-# make directorries and run the san binary
-/bin/rm APparams.dat init*dat current*.dat
-/bin/rm run1/APparams.dat run1/init*dat run1/current*.dat
-#
-
